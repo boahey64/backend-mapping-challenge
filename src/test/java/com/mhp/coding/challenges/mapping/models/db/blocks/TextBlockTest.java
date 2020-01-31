72019ed8d78f8f04cab3d0f59087a4bad613088e
roman <roman.richter.git.user>
2020-01-31 16:40:49 +0100

    added one more test to test class TextBlockTest

--- src/test/java/com/mhp/coding/challenges/mapping/models/db/blocks/TextBlockTest.java
+++ src/test/java/com/mhp/coding/challenges/mapping/models/db/blocks/TextBlockTest.java
@@ -6,28 +6,48 @@ import com.mhp.coding.challenges.mapping.models.dto.blocks.TextBlockDto;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class TextBlockTest {
-    private TextBlock objectUnderTest = aTextBlock(5);
-
     @Test
     public void map_text_block_to_text_block_dto() {
         TextBlockDto expected = aTextBlockDto(5);
 
-        TextBlockDto actual = callMapAndSetType();
+        TextBlock objectUnderTest = aTextBlock(5);
+
+        TextBlockDto actual = callMapAndSetType(objectUnderTest);
 
         assertEquals(expected.getSortIndex(), actual.getSortIndex());
         assertEquals(expected.getText(), actual.getText());
     }
 
-    private TextBlockDto callMapAndSetType() {
+    @Test
+    public void map_text_block_without_text_field_to_text_block_dto() {
+        TextBlockDto expected = aTextBlockDtoWithoutText(4);
+
+        TextBlock objectUnderTest = aTextBlockWithoutText(4);
+
+        TextBlockDto actual = callMapAndSetType(objectUnderTest);
+
+        assertEquals(expected.getSortIndex(), actual.getSortIndex());
+        assertEquals(expected.getText(), actual.getText());
+        assertNull(actual.getText());
+    }
+
+    private TextBlockDto callMapAndSetType(TextBlock textBlock) {
         return new BlockDtoMapperGeneric<>(TextBlockDto.class).getType(
-                objectUnderTest.map(null));
+                textBlock.map(null));
     }
 
     private TextBlockDto aTextBlockDto(int sortIndex) {
         TextBlockDto textBlockDto = new TextBlockDto(sortIndex);
         textBlockDto.setText("Dies ist ein Text");
+
+        return textBlockDto;
+    }
+
+    private TextBlockDto aTextBlockDtoWithoutText(int sortIndex) {
+        TextBlockDto textBlockDto = new TextBlockDto(sortIndex);
 
         return textBlockDto;
     }
@@ -38,4 +58,11 @@ public class TextBlockTest {
         textBlock.setText("Dies ist ein Text");
         return textBlock;
     }
+
+    private TextBlock aTextBlockWithoutText(int sortIndex) {
+        TextBlock textBlock = new TextBlock();
+        textBlock.setSortIndex(sortIndex);
+        return textBlock;
+    }
+
 }
---
+++ src/test/java/com/mhp/coding/challenges/mapping/models/db/blocks/VideoBlockTest.java
@@ -0,0 +1,12 @@
+package com.mhp.coding.challenges.mapping.models.db.blocks;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VideoBlockTest {
+    @Test
+    public void map_video_block_to_video_block_dto() {
+
+    }
+}
