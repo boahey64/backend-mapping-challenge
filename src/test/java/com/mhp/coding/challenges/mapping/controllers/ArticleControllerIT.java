ec2db405acb8f6cacd3d3b68aeedf55ed071e902
roman <roman.richter.git.user>
2020-02-02 15:23:33 +0100

    Add second controller test

--- src/test/java/com/mhp/coding/challenges/mapping/controllers/ArticleControllerIT.java
+++ src/test/java/com/mhp/coding/challenges/mapping/controllers/ArticleControllerIT.java
@@ -13,11 +13,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -35,21 +38,10 @@ public class ArticleControllerIT {
 
     private String path = "/article";
 
-    @Before
-    public void setUp() {
-        List<ArticleDto> articleList = new ArrayList<>();
-        ArticleDto articleDto = new ArticleDto();
-        articleDto.setId(3L);
-        articleDto.setTitle("the title");
-        articleDto.setDescription("the description");
-        articleDto.setAuthor("Max Mustermann");
-        articleList.add(articleDto);
-
-        when(articleService.list()).thenReturn(articleList);
-    }
-
     @Test
     public void call_list_endpoint() throws Exception {
+        when(articleService.list()).thenReturn(anArticleList());
+
         mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get(path))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
@@ -58,6 +50,37 @@ public class ArticleControllerIT {
                 .andExpect(jsonPath("$[0].title", equalTo("the title")))
                 .andExpect(jsonPath("$[0].description", equalTo("the description")))
                 .andExpect(jsonPath("$[0].author", equalTo("Max Mustermann")))
-                ;
+        ;
     }
+
+    @Test
+    public void call_item_endpoint() throws Exception {
+        when(articleService.articleForId(3L)).thenReturn(anArticleDto());
+
+        mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get(path + "/3"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.id", equalTo(3)))
+                .andExpect(jsonPath("$.title", equalTo("the title")))
+                .andExpect(jsonPath("$.description", equalTo("the description")))
+                .andExpect(jsonPath("$.author", equalTo("Max Mustermann")))
+        ;
+    }
+
+    private List<ArticleDto> anArticleList() {
+        List<ArticleDto> articleList = new ArrayList<>();
+        ArticleDto articleDto = anArticleDto();
+        articleList.add(articleDto);
+        return articleList;
+    }
+
+    private ArticleDto anArticleDto() {
+        ArticleDto articleDto = new ArticleDto();
+        articleDto.setId(3L);
+        articleDto.setTitle("the title");
+        articleDto.setDescription("the description");
+        articleDto.setAuthor("Max Mustermann");
+        return articleDto;
+    }
+
 }
